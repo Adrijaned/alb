af3eb16c64219e0eaa38fe3274921c06faf3ed95
Adrijaned <adrijaned@gmail.com>
2019-07-04 21:33:26 +0200

    Added string parsing test, fixed StringStore tests

--- tests/src/include/Lexer.cpp
+++ tests/src/include/Lexer.cpp
@@ -8,6 +8,8 @@
 
 #include <gtest/gtest.h>
 #include <include/Lexer.hpp>
+#include <absl/strings/match.h>
+#include <include/Resources.hpp>
 
 TEST(tokenizer_tests, tokenizer_tests_constructor_Test) {
   ASSERT_NO_THROW(alb_lang::Lexer{});
@@ -40,4 +42,22 @@ TEST(tokenizer_tests, tokenizer_tests_utf8_parsing_multiline_comment_Test) {
   EXPECT_EQ(tokens[0]->getTextContents(), std::string{"BEGIN"});
   EXPECT_EQ(tokens[1]->getTextContents(), std::string{"."});
   EXPECT_EQ(tokens[2]->getTextContents(), std::string{"END"});
+}
+
+TEST(tokenizer_tests, tokenizer_tests_utf8_parsing_simple_string_Test) {
+  char testData[] = "BEGIN .\nvar {testString \"randomText\"}\nEND";
+  std::vector<alb_lang::Token*> tokens;
+  alb_lang::Lexer::tokenizeUTF8(testData, sizeof(testData), tokens);
+  EXPECT_EQ(tokens.size(), 8);
+  ASSERT_GE(tokens.size(), 5);
+  EXPECT_EQ(tokens[0]->getTextContents(), std::string{"BEGIN"});
+  EXPECT_EQ(tokens[1]->getTextContents(), std::string{"."});
+  EXPECT_EQ(tokens[2]->getTextContents(), std::string{"var"});
+  EXPECT_EQ(tokens[3]->getTextContents(), std::string{"{"});
+  EXPECT_EQ(tokens[4]->getTextContents(), std::string{"testString"});
+  ASSERT_GE(tokens.size(), 7);
+  EXPECT_TRUE(absl::StartsWith(tokens[5]->getTextContents(), "::internal::strings::"));
+  EXPECT_EQ(alb_lang::Resources::stringStore().getString(tokens[5]->getTextContents()), std::string{"randomText"});
+  EXPECT_EQ(tokens[6]->getTextContents(), std::string{"}"});
+  EXPECT_EQ(tokens[7]->getTextContents(), std::string{"END"});
 }
--- tests/src/include/StringStore.cpp
+++ tests/src/include/StringStore.cpp
@@ -8,20 +8,17 @@
 
 #include <gtest/gtest.h>
 #include <include/StringStore.hpp>
-
-TEST(stringStore_tests, stringStore_tests_constructor_Test) {
-  ASSERT_NO_THROW(alb_lang::StringStore{});
-}
+#include <include/Resources.hpp>
 
 TEST(stringstore_tests, stringstore_tests_counter_incrementation_Test) {
-  alb_lang::StringStore stringStore {};
+  alb_lang::StringStore stringStore = alb_lang::Resources::stringStore();
   ASSERT_EQ(stringStore.getStringLiteralCount(), 0);
   stringStore.storeString("test");
   ASSERT_EQ(stringStore.getStringLiteralCount(), 1);
 }
 
 TEST(stringstore_tests, stringstore_tests_string_retrieval_Test) {
-  alb_lang::StringStore stringStore {};
+  alb_lang::StringStore stringStore = alb_lang::Resources::stringStore();
   std::string id = stringStore.storeString("test1");
   stringStore.storeString("test2");
   const std::string &returnedString = stringStore.getString(id);
